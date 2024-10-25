d8303f567e67ceaf23fb029cc4c22d8efc07bfcb
Xin1337 <drlftg0d321@gmail.com>
2024-10-25 14:10:20 +0800

    upd

--- Project.asm
+++ Project.asm
@@ -11,6 +11,7 @@ input1 db ?
 input2 db ?
 s dw ?
 f dw ?
+onefourth_called db 0 ; Flag to check if onefourth was called before
 
 .code
 begin proc near
@@ -129,32 +130,25 @@ grats:
     jmp exit
 
 oops: 
-    ; Set cursor position for "Oops Incorrect" message
-    mov ah, 02h
-    mov bh, 00h
-    mov dh, 0ch ; row position for "Oops Incorrect"
-    mov dl, 18h ; column position
+    mov ah,02h
+    mov bh,00h
+    mov dh,0ch
+    mov dl,18h
     int 10h
-
-    ; Display "Oops Incorrect" message
-    mov ah, 09h
-    lea dx, wrong
-    int 21h
-
-    ; Set cursor position for "Try Again!" message
-    mov ah, 02h
-    mov bh, 00h
-    mov dh, 0eh ; row position for "Try Again!"
-    mov dl, 18h ; column position
-    int 10h
-
-    ; Display "Try Again!" message
-    mov ah, 09h
-    lea dx, try
+    
+    mov ah,09h
+    lea dx,try
     int 21h
     
-    ; Call baso procedure to perform additional screen manipulation
-    call baso
+    ; Call onefourth procedure
+    call onefourth
+    
+    ; Check if onefourth was called before
+    cmp onefourth_called, 1
+    je baso
+    
+    ; Set the flag to indicate onefourth has been called
+    mov onefourth_called, 1
     
     ; Call timer procedure for delay
     call timer
@@ -222,6 +216,12 @@ baso proc near
     mov bh, 27h
     mov ch, 8d
     mov dh, 8d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 9d
+    mov dh, 9d
     int 10h
     
     mov ax, 600h
@@ -533,6 +533,391 @@ baso proc near
 
   ret
 baso endp
+
+onefourth proc near
+    ; Clear the screen
+    mov ax, 0600h
+    mov bh, 07h
+    mov cx, 0000h
+    mov dx, 184fh
+    int 10h
+
+    ; Set cursor position
+    mov ah, 02h
+    mov bh, 00h
+    mov dh, 15d
+    mov dl, 24d
+    int 10h
+
+    ; Display text or graphics
+    mov ax, 600h
+    mov bh, 16h
+    mov dh, 15d
+    mov ch, 13d
+    mov cl, 24d
+    mov dl, 50d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 23d
+    mov dh, 16d
+    mov dl, 23d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 7d
+    mov dh, 7d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 8d
+    mov dh, 8d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 9d
+    mov dh, 9d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 10d
+    mov dh, 10d
+    int 10h
+    
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 11d
+    mov dh, 11d
+    int 10h
+    
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 12d
+    mov dh, 12d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 13d
+    mov dh, 13d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 14d
+    mov dh, 14d
+    int 10h
+
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 15d
+    mov dh, 15d
+    int 10h
+
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 24d
+    mov dh, 16d
+    mov dl, 24d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 25d
+    mov dh, 16d
+    mov dl, 25d
+    int 10h
+    
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 26d
+    mov dh, 16d
+    mov dl, 26d
+    int 10h
+    
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 27d
+    mov dh, 16d
+    mov dl, 27d
+    int 10h
+    
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 28d
+    mov dh, 16d
+    mov dl, 28d
+    int 10h
+    
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 29d
+    mov dh, 16d
+    mov dl, 29d
+    int 10h
+    
+    
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 30d
+    mov dh, 16d
+    mov dl, 30d
+    int 10h
+    
+    
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 31d
+    mov dh, 16d
+    mov dl, 31d
+    int 10h
+    
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 32d
+    mov dh, 16d
+    mov dl, 32d
+    int 10h
+
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 33d
+    mov dh, 16d
+    mov dl, 33d
+    int 10h
+    
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 33d
+    mov dh, 16d
+    mov dl, 33d
+    int 10h
+    
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 34d
+    mov dh, 16d
+    mov dl, 34d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 35d
+    mov dh, 16d
+    mov dl, 35d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 36d
+    mov dh, 16d
+    mov dl, 36d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 37d
+    mov dh, 16d
+    mov dl, 37d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 38d
+    mov dh, 16d
+    mov dl, 38d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 39d
+    mov dh, 16d
+    mov dl, 39d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 40d
+    mov dh, 16d
+    mov dl, 40d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl,41d
+    mov dh, 16d
+    mov dl, 41d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 42d
+    mov dh, 16d
+    mov dl, 42d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 43d
+    mov dh, 16d
+    mov dl, 43d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 44d
+    mov dh, 16d
+    mov dl, 44d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 45d
+    mov dh, 16d
+    mov dl, 45d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 46d
+    mov dh, 16d
+    mov dl, 47d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 48d
+    mov dh, 16d
+    mov dl, 48d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 49d
+    mov dh, 16d
+    mov dl, 49d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 50d
+    mov dh, 16d
+    mov dl, 50d
+    int 10h
+        
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 16d
+    mov cl, 51d
+    mov dh, 16d
+    mov dl, 51d
+    int 10h
+    
+    ;length left
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 7d
+    mov dh, 7d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 8d
+    mov dh, 8d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 9d
+    mov dh, 9d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 10d
+    mov dh, 10d
+    int 10h
+    
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 11d
+    mov dh, 11d
+    int 10h
+    
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 12d
+    mov dh, 12d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 13d
+    mov dh, 13d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 14d
+    mov dh, 14d
+    int 10h
+    
+    mov ax, 600h
+    mov bh, 27h
+    mov ch, 15d
+    mov dh, 15d
+    int 10h
+
+ret
+onefourth endp
 
 begin endp
 end begin
